21fd282b3ce553733a345f80eb4ca679bba01b46
Théo VIDAL <theo.vidal@ensta-paris.fr>
2024-10-08 15:38:49 +0200

    wip: add a subscription example (with GPS data)

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Aquabot Challenge - Our submission
 
 Made by Théo Vidal and Yao Kouassi
+
+## Scripts
+
+All scripts are located in the `scripts/` folder. They are utilities to build and run the project :
+
+- [build](scripts/build): build all packages located in `~/vrx_ws`, including the project
+- [run](scripts/run): run the node of this project
+- [build_run](scripts/build_run): combination of both
+- [sim](scripts/sim): start the simulation window
+- [rviz2](scripts/rviz2): launch rviz2 utility, to visualize the scene and topics
+- [topics](scripts/topics): interact with ROS2 topics
--- src/node.py
+++ src/node.py
@@ -2,40 +2,64 @@ import rclpy
 from rclpy.node import Node
 import time
 
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Float64MultiArray, Int64, String
+from sensor_msgs.msg import NavSatFix
 
-
-class Publisher(Node):
-
+class Aquabot(Node):
+    # Note: this function shouldn't block or call another one,
+    # otherwise callbacks are never triggered, so we don't receive any message
     def __init__(self):
-        super().__init__('minimal_subscriber')
-        self.publisher_ = self.create_publisher(
+        super().__init__('aquabot')
+        self.left_thrust = self.create_publisher(
             Float64,
             '/aquabot/thrusters/left/thrust',
             5)
-        self.get_logger().info('Subscriber started')
-        self.run()
+        self.acoustics = self.create_subscription(
+            NavSatFix,
+            '/aquabot/sensors/gps/gps/fix',
+            self.acoustics_callback,
+            10
+        )
 
-    def run(self):
-        while(1):
-            time.sleep(1)
-            msg = Float64()
-            msg.data = 500.0
-            self.publisher_.publish(msg)
-            self.get_logger().info('Publishing new thrust')
+        self.get_logger().info('Script started')
+
+        # Timer to publish thrust every 1 second
+        self.timer = self.create_timer(1.0, self.publish_thrust)
+
+    def acoustics_callback(self, msg):
+        self.get_logger().info(f'Latitude: {msg.latitude} ; Longitude : {msg.longitude} ; Altitude = {msg.altitude}')
+
+    def publish_thrust(self):
+        time.sleep(1)
+        msg = Float64()
+        msg.data = 500.0
+        self.left_thrust.publish(msg)
+        self.get_logger().info('Publishing new thrust')
+
+    # Reset the parameters conveniently to avoid any problem with the bot
+    def shutdown(self):
+        msg = Float64()
+        msg.data = 0.0
+        self.left_thrust.publish(msg)
+        self.get_logger().info('Kindly shutting down the bot...')
+
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    pub = Publisher()
+    bot = Aquabot()
 
-    rclpy.spin(pub)
+    # Interrupt the execution nicely when pressing Ctrl+C
+    try:
+        rclpy.spin(bot)
+    except KeyboardInterrupt:
+        bot.shutdown()
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    pub.destroy_node()
+    bot.destroy_node()
     rclpy.shutdown()
 
 
